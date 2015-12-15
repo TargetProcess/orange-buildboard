4285a518f0010179a6c8ff57dd8da67049635948
Alex Fomin <ShurickFomin@gmail.com>
2015-12-15 20:34:31 +0200

    Change account config

--- server/tools.js
+++ server/tools.js
@@ -1,63 +1,23 @@
-var findToolByTypeAndId = function (type, id) {
+var findToolById = function (id) {
     var tools = Meteor.settings.tools;
-    var tool = _.chain(tools)
-        .find(tool => tool.type === type)
-        .pick('tools').value();
-    return _.find(tool.tools, tool=>tool.id === id);
-};
-class ToolBase {
-    constructor(config, toolType) {
-        if (config) {
-            var tool = findToolByTypeAndId(toolType, config.toolId);
-            this._url = Meteor.settings.url + ':' + tool.port;
-            this._accountToken = config.accountToken;
-        }
-    }
-
-    _get(resource) {
-        if (this._url) {
-            var url = this._url + `/${resource}?token=${this._accountToken}&take=1000`;
-
-            var result = [];
-            while (url) {
-                var items = HTTP.get(url).data;
-                result = result.concat(items[resource]);
-                url = items.next;
-            }
-            return result;
-        }
-        else {
-            return [];
-        }
-    }
-}
-
-PMTool = class PMTool extends ToolBase {
-    constructor(config) {
-        super(config, 'PMTool');
-    }
-
-    getTasks() {
-        return this._get('tasks');
-    }
+    return _.find(tools, tool => tool.id === id);
 };
 
-CodeTool = class CodeTool extends ToolBase {
-    constructor(config) {
-        super(config, 'CodeTool');
-    }
 
-    getBranches() {
-        return this._get('branches');
-    }
-};
+Tool = class Tool {
 
-BuildTool = class BuildTool extends ToolBase {
-    constructor(config) {
-        super(config, 'BuildTool');
-    }
+    static getAll(config, resource) {
 
-    getBuilds() {
-        return this._get('builds');
+        var urlBase = config.url || findToolById(config.id).url;
+
+        var url = `${urlBase}/${resource}?token=${config.token}&take=1000`;
+
+        var result = [];
+        while (url) {
+            var items = HTTP.get(url).data;
+            result = result.concat(items[resource]);
+            url = items.next;
+        }
+        return result;
     }
 };

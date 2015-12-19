de6a6930861e9c11295ca9a13f9c4808f4ea006a
Alex Fomin <ShurickFomin@gmail.com>
2015-12-19 14:34:46 +0200

    Move mappings to separate configs

--- client/collections.js
+++ client/collections.js
@@ -5,51 +5,20 @@ Items = new Meteor.Collection(null);
 _.chain(collections)
     .values()
     .filter(collection=>collection.opposite)
-    .each(collecion=> {
-        var oppositeCollection = collections[collecion.opposite];
-        var itemName = collecion.item;
-        var oppositeItemName = oppositeCollection.item;
-        let itemId = itemName + '.id';
-        let oppositeId = oppositeItemName + '.id';
+    .each(collection=> {
 
-        collecion.collection
+        let bind = function (actionName) {
+            return function (item) {
+                _.each(collection.mappings, mapping=> {
+                    mapping.bind(mapping, collection, item, actionName, item.account);
+                })
+            }
+        };
+        collection.collection
             .find({})
             .observe({
-                added: function (item) {
-                    var opposites = collecion.mapToOpposite(item.account, [item]);
-                    opposites.forEach(opposite=> {
-
-                        Items.upsert({
-                            [itemId]: item.id,
-                            [oppositeId]: opposite.id
-                        }, {$set: {[itemName]: item, [oppositeItemName]: opposite}});
-
-                        Items.remove({
-                            [oppositeId]: opposite.id,
-                            [itemName]: {$exists: false}
-                        });
-                    });
-
-                    if (opposites.count() == 0) {
-                        Items.insert({[itemName]: item});
-                    }
-                    else {
-                        Items.remove({
-                            [itemId]: item.id,
-                            [oppositeItemName]: {$exists: false}
-                        })
-                    }
-                },
-                changed: function (item) {
-                    Items.update({[itemId]: item.id}, {$set: {[itemName]: item}}, {multi: true});
-                },
-                removed: function (item) {
-                    Items.remove({
-                        [itemId]: item.id,
-                        [oppositeItemName]: {$exists: false}
-                    });
-
-                    Items.update({[itemId]: item.id}, {$unset: {[itemName]: item}}, {multi: true})
-                }
+                added: bind('added'),
+                changed: bind('changed'),
+                removed: bind('removed')
             })
     });

f33459a88667486aa06f6f8f8e377c472fb79c56
konstantin <krivlenia@targetprocess.com>
2015-12-21 17:27:32 +0300

    rework routing

--- packages/buildboard-account/client/account-item.js
+++ packages/buildboard-account/client/account-item.js
@@ -1,6 +1,8 @@
 Template.accountItem.helpers({
     isOwner() {
-       return this.owners.indexOf(Meteor.userId()) !== -1;
-   }
+        return this.owners.indexOf(Meteor.userId()) !== -1;
+    },
+    account() {
+        return {account: this.id};
+    }
 });
-

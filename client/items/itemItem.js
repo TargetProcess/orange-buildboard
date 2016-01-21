bebe1d56f55dd61f86d5043438235f1ce340913a
Uladzimir Abramchuk <abramchuk@targetprocess.com>
2016-01-21 17:38:40 +0300

    Display task, state, branch, pull request and last build in work item list

--- client/items/itemItem.js
+++ client/items/itemItem.js
@@ -1,12 +1,24 @@
-Template.itemItem.helpers({
-    getProperty(object, prop) {
-        return object && object[prop] || '';
-    },
+function getProperty(object, prop) {
+    return object && object[prop] || '';
+}
 
-    first(items, prop) {
-        return items && items[0] && items[0][prop] || '';
-    }
-});
+function state(item, prop) {
+    return getProperty(first(item.tasks, 'state'), prop);
+}
+
+function first(items, prop) {
+    return items && items[0] && items[0][prop] || '';
+}
+
+function pullRequest(item, prop) {
+    return getProperty(first(item.branches, 'pullRequest'), prop);
+}
+
+function lastBuild(item, prop) {
+    return getProperty(first(item.branches, 'lastBuild'), prop);
+}
+
+Template.itemItem.helpers({getProperty, state, first, pullRequest, lastBuild});
 Template.itemItem.events({
     'click tr'(){
         Router.go('/' + this.parent.account + '/items/' + this.item._id);
